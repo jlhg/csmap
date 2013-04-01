c0bcdaefcf53d5050f039e94e9566f43a4e8aade
Jian-Long Huang <jianlong@ntu.edu.tw>
2013-04-02 00:41:01 +0800

    A prototype with no test

--- csmap.py
+++ csmap.py
@@ -2,27 +2,61 @@
 
 import re
 import sys
+import bisect
 import tarfile
 
 
 class WigData:
-    def __init__(self):
-        self.chrom = None
-        self.start = None
+    def __init__(self, fileobj):
+        self.file = fileobj
+        # self.chrom = None
+        self.starts = []
+        self.stop = {}
+        self.offset = {}
+
+    def set_offset(self, start, stop, offset):
+        self.starts.append(start)
+        self.stop.update({start: stop})
+        self.offset.update({start: offset})
+
+    def map(self, start, end):
+        i = bisect.bisect(self.starts, start)
+        if i == 0:
+            return None         # No data
+        elif end > self.stop.get(self.starts[i - 1]):
+            return None         # Stop is out of range
+        else:
+            return self.get_scores(start, end)
+
+    def get_scores(self, start, end):
+        self.file.seek(self.offset.get(start))
+        return self.file.read((end - start + 1) * 6 - 1).split(b'\n')
 
 
 class WigLister:
     def __init__(self, filename):
-        self.wiglist = {}
+        self.wig_data_list = {}
+        self.chroms = []
 
-        tar_file = tarfile.open(argv)
         header = re.compile(b'# (.+) chrom=(.+)')
 
-        header = index_file.readline()
-        with open()
-        self.chrom = None
-        self.wigcontent = WigContent()
+        tar_file = tarfile.open(filename, 'r:gz')
+        for line in tar_file.extractfile('index'):
+            if header.match(line):
+                wig_filename = header.match(line).group(1)
+                chrom = header.match(line).group(2)
+                self.chroms.append(chrom)
+                wig_data = WigData(tar_file.extractfile(wig_filename))
+                self.wig_data_list.update({chrom: wig_data})
+            else:
+                data = line.rstrip().split(' ')
+                self.wig_data_list.get(chrom).set_offset(*data)
 
+    def get_chroms(self):
+        return self.chroms
+
+    def map(self, chrom, start, end):
+        return self.wig_data_list.get(chrom).map(start, end)
 
 
 def main(argvs):
@@ -31,40 +65,38 @@ def main(argvs):
         sys.exit('Usage: csmap.py <input.fa> <scores.tar.gz> <output.txt>')
 
     # Parse tar file
-    wig_format = re.compile(b'fixedStep chrom=(.+) start=(\d+) step=(\d+)')
-    wig_index_format = re.compile(b'chrom=(.+)')
+    # wig_format = re.compile(b'fixedStep chrom=(.+) start=(\d+) step=(\d+)')
+    # wig_index_format = re.compile(b'chrom=(.+)')
 
-    score_data = WigLister(argv[1])
+    score_data = WigLister(argvs[1])
     # score_data = WigLister(tarfile.open(argvs[1], 'r:gz'))
 
-    score_data = WigIndex(score_tarfile.extractfile('index'))
+    # members = score_tarfile.getmembers()
 
-    members = score_tarfile.getmembers()
+    # chr_score_files = {}
+    # chr_starts = {}
+    # chr_steps = {}
+    # chr_offset = {}
 
-    chr_score_files = {}
-    chr_starts = {}
-    chr_steps = {}
-    chr_offset = {}
+    # score_files =
 
-    score_files =
+    # for member in members:
+    #     score_file = score_tarfile.extractfile(member.name)
+    #     header = score_file.readline()
 
-    for member in members:
-        score_file = score_tarfile.extractfile(member.name)
-        header = score_file.readline()
+    #     if wig_format.match(header):
+    #         chr_score_files.update({chr_name: WigContent(wig_format.match(header), score_file)})
+    #     elif wig_index_format.match(header):
+    #         chr_score_
 
-        if wig_format.match(header):
-            chr_score_files.update({chr_name: WigContent(wig_format.match(header), score_file)})
-        elif wig_index_format.match(header):
-            chr_score_
+    #     if header is None:
+    #         sys.exit('Format error in the score file, please check it.')
 
-        if header is None:
-            sys.exit('Format error in the score file, please check it.')
-
-        chr_name = header.group(1).decode('UTF-8')
-        chr_score_files.update({chr_name: score_file})
-        chr_starts.update({chr_name: int(header.group(2).decode('UTF-8'))})
-        chr_steps.update({chr_name: int(header.group(3).decode('UTF-8'))})
-        chr_offset.update({chr_name: score_file.tell()})
+    #     chr_name = header.group(1).decode('UTF-8')
+    #     chr_score_files.update({chr_name: score_file})
+    #     chr_starts.update({chr_name: int(header.group(2).decode('UTF-8'))})
+    #     chr_steps.update({chr_name: int(header.group(3).decode('UTF-8'))})
+    #     chr_offset.update({chr_name: score_file.tell()})
 
     # Start mapping
     fa_header = re.compile('.+range=(.+):(\d+)-(\d+)\s.+')
@@ -82,22 +114,26 @@ def main(argvs):
                 chr_start = int(header.group(2))
                 chr_end = int(header.group(3))
 
-                if chr_name not in chr_score_files:
-                    print('Chromosome not found in score files: ' + seq_name)
+                if chr_name not in score_data.get_chroms():
+                    print('No chromosome is found in score files: ' + seq_name)
                     continue
 
-                if chr_start < chr_starts.get(chr_name):
-                    print('No score data ' + seq_name)
-                    continue
+                scores = score_data.map(chr_name, chr_start, chr_end)
 
-                chr_score_files.get(chr_name).seek(chr_offset.get(chr_name) + (chr_start - 1) * 6)
-                scores = map(float, chr_score_files.get(chr_name).read((chr_end - chr_start + 1) * 6 - 1).split(b'\n'))
+                # if chr_start < chr_starts.get(chr_name):
+                #     print('No score data ' + seq_name)
+                #     continue
 
-                score_avg = sum(scores) / len(scores)
+                # chr_score_files.get(chr_name).seek(chr_offset.get(chr_name) + (chr_start - 1) * 6)
+                # scores = map(float, chr_score_files.get(chr_name).read((chr_end - chr_start + 1) * 6 - 1).split(b'\n'))
 
-                fo.write(seq_name + '\t' + str(score_avg) + '\n')
-                fo.write('scores:' + '\n' + '\n'.join(map(str, scores)) + '\n')
-                fo.flush()
+                if scores is None:
+                    print('No score data is found: ' + seq_name)
+                else:
+                    score_avg = sum(scores) / len(scores)
+                    fo.write(seq_name + '\t' + str(score_avg) + '\n')
+                    fo.write('scores:' + '\n' + '\n'.join(map(str, scores)) + '\n')
+                    fo.flush()
 
 if __name__ == '__main__':
     main(sys.argv[1:])
